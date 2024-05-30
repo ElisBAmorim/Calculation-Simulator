ce20cc864bdd9a7a82a8633e20104ec22ad63c09
elisangelabrazamorim <elisangelabrazamorim@hotmail.com>
2024-05-30 16:26:31 -0300

    Adding the Fields on the Canvas

--- CalculationSimulatorWeb/src/app/app.module.ts
+++ CalculationSimulatorWeb/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CalculateCdbComponent } from './componentes/calculations/calculate-cdb/calculate-cdb.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    CalculateCdbComponent
   ],
   imports: [
     BrowserModule,
---
+++ CalculationSimulatorWeb/src/app/componentes/calculations/calculate-cdb/calculate-cdb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate-cdb',
+  templateUrl: './calculate-cdb.component.html',
+  styleUrls: ['./calculate-cdb.component.css']
+})
+export class CalculateCdbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
