f55e32805ae4c8c9c08a32992c5c086bcd9ca744
elisangelabrazamorim <elisangelabrazamorim@hotmail.com>
2024-06-01 15:19:34 -0300

    Refactor

--- CalculationSimulatorWeb/src/app/app.module.ts
+++ CalculationSimulatorWeb/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CalculateCdbComponent } from './componentes/calculations/calculate-cdb/calculate-cdb.component';
-import { CalculationResultComponent } from './componentes/calculation-result/calculation-result.component';
 import { ResultCdbComponent } from './componentes/calculations/result-cdb/result-cdb.component';
 
 @NgModule({
@@ -15,8 +14,7 @@ import { ResultCdbComponent } from './componentes/calculations/result-cdb/result
     AppComponent,
     CabecalhoComponent,
     RodapeComponent,
-    CalculateCdbComponent,
-    CalculationResultComponent,
+    CalculateCdbComponent,   
     ResultCdbComponent
   ],
   imports: [
