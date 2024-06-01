e7cfc2bbfee44d5eec9a0c92f10977194de2e02f
elisangelabrazamorim <elisangelabrazamorim@hotmail.com>
2024-05-31 22:50:32 -0300

    Displaying Calculation and Result

--- CalculationSimulatorWeb/src/app/app-routing.module.ts
+++ CalculationSimulatorWeb/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculateCdbComponent } from './componentes/calculations/calculate-cdb/calculate-cdb.component';
+import { CalculationResultComponent } from './componentes/calculation-result/calculation-result.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{
+  //  path: '',
+  //  redirectTo: 'simular-calulo-cdb',
+  //  pathMatch: 'full'
+  //},
+  {
+    path: 'simular-calulo-cdb',
+    component: CalculateCdbComponent
+  },
+  {
+    path: 'resultado-calulo-cdb',
+    component: CalculationResultComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
